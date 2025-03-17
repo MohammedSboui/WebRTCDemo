26c23766addb9866b83a5356893f05613862b78d
Rim Chebbi <RimC@cred.local>
2025-03-17 10:48:43 +0100

    migrate app to angular 19

---
+++ src/app/models/peerData.ts
@@ -0,0 +1,20 @@
+export interface PeerData {
+    id: string;
+    data: any;
+  }
+  
+  export interface UserInfo {
+    userName: string;
+    connectionId: string;
+  }
+  
+  export interface SignalInfo {
+    user: string;
+    signal: any;
+  }
+  
+  export interface ChatMessage {
+    own: boolean;
+    message: string;
+  }
+  
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RtcService } from '../rtc.service';
+import { UserInfo } from '../models/peerData';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-user-list',
+  imports: [CommonModule],
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+
+  @Output() userSelected: EventEmitter<UserInfo> = new EventEmitter();
+
+  public users$!: Observable<Array<UserInfo>>;
+
+
+  constructor(private rtcService: RtcService) { }
+
+  ngOnInit() {
+    this.users$ = this.rtcService.users$;
+  }
+
+  public userClicked(user: UserInfo) {
+    this.userSelected.emit(user);
+  }
+
+}
